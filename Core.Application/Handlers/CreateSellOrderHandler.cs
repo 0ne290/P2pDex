c9654c6c20d9106b3f045a085ada5f010ca7b13a
Алексей <97665299+0ne290@users.noreply.github.com>
2024-11-29 22:33:21 +0800

    Update CreateSellOrderHandler.cs

--- Core.Application/Handlers/CreateSellOrderHandler.cs
+++ Core.Application/Handlers/CreateSellOrderHandler.cs
@@ -20,20 +20,22 @@ public class CreateSellOrderHandler
     public async Task<Result> Handle(CreateSellOrderCommand request)
     {
         var validationResult = await _validator.ValidateAsync(request);
+        
         if (!validationResult.IsValid)
             return Result.Fail(validationResult.ToString());
 
         var seller = await _traderStorage.TryGetByGuid(request.SellerGuid);
+        
         if (seller == null)
             return Result.Fail($"Trader with guid \"{request.SellerGuid}\" does not exist.");
 
         var feeFromSeller = await _feeCalculator.Calculate(request.CryptoAmount);
         var order = new SellOrder(Guid.NewGuid().ToString(), request.Crypto, request.CryptoAmount, request.Fiat,
             request.CryptoToFiatExchangeRate, request.PaymentMethodInfo, seller, feeFromSeller);
+        
         await _orderStorage.Add(order);
 
         return Result.Ok();
-
     }
 
     private readonly IValidator<CreateSellOrderCommand> _validator;
