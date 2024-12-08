ad2e3b68dd80f55ea67d06fbcbdcfeaec7f12a2a
Алексей <97665299+0ne290@users.noreply.github.com>
2024-12-08 23:04:08 +0800

    Update Extensions.cs

--- Infrastructure/Extensions.cs
+++ Infrastructure/Extensions.cs
@@ -15,12 +15,28 @@ public static class Extensions
         if (!Array.Exists(unlockedAccounts, ua => ua == exchangerAccountAddress))
             throw new Exception("Failed to unlock exchanger account. Address and/or password are invalid.");
         
-        services.AddKeyedSingleton<Web3>("Singleton", () =>
-            new TransferTransactionFeeTracker(sp.GetRequiredKeyedService<Web3>("Singleton"), feeUpdateIntervalInMs));
+        services.AddKeyedSingleton<Web3>("Singleton", () => new Web3(new ManagedAccount(exchangerWalletAddress, walletPassword)));
+        services.AddKeyedScoped<Web3>("Scoped", () => new Web3(new ManagedAccount(exchangerWalletAddress, walletPassword)));
         
         services.AddSingleton<TransferTransactionFeeTracker>(sp =>
             new TransferTransactionFeeTracker(sp.GetRequiredKeyedService<Web3>("Singleton"), feeUpdateIntervalInMs));
+
+        services.AddScoped<IBlockchain, EthereumBlockchain>(sp => new EthereumBlockchain(sp.GetRequiredKeyedService<Web3>("Scoped"), sp.GetRequiredService<TransferTransactionFeeTracker>()));
         
         return services;
+    }
+
+    public static async Task<IServiceCollection> AddPersistence(this IServiceCollection services, string connectionString)
+    {
+        var testDbContext = new P2PDbContext((new DbContextOptionsBuilder()).UseSqlite(connectionString).Options);
+        await testDbContext.Database.EnsureCreatedAsync();
+        
+        services.AddDbContext<P2PDbContext>(options => options.UseSqlite(connectionString));
+
+        services.AddScoped(IRepository, Repository);
+        
+        services.AddScoped<IUnitOfWork, UnitOfWork>();
+
+        return services
     }
 }
