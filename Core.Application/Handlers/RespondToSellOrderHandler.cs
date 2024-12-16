638e32068887ce927e529df35aa2e2318965a8ea
Алексей <97665299+0ne290@users.noreply.github.com>
2024-12-16 22:38:39 +0800

    Update RespondToSellOrderHandler.cs

--- Core.Application/Handlers/RespondToSellOrderHandler.cs
+++ Core.Application/Handlers/RespondToSellOrderHandler.cs
@@ -7,6 +7,21 @@ public class RespondToSellOrderHandler : IRequestHandler<RespondToSellOrderComma
 {
     public Task<CommandResult> Handle(RespondToSellOrderCommand request, CancellationToken _)
     {
-        throw new NotImplementedException();
+        var ethereumAccountAddressRegex = new Regex("^0x[0-9a-fA-F]{40}$");
+    
+        var order = await _unitOfWork.Repository.TryGetByGuid<SellOrder>(request.OrderGuid);
+        order.Respond(request.BuyerGuid, request.BuyerAccountAddress);
+
+        if (order == null)
+            throw new InvariantViolationException("Order does not exists.");
+        if (!await _unitOfWork.Repository.Exists<Trader>(t => t.Guid == request.BuyerGuid))
+            throw new InvariantViolationException("Buyer does not exists.");
+        // TODO: вынести эту проверку в сущность.
+        if (!ethereumAccountAddressRegex.Match(request.BuyerAccountAddress))
+            throw new InvariantViolationException("Buyer account address is invalid.");
+    
+        await _unitOfWork.Save();
+
+        return new CommansResult(new { guid = order.Guid, status = order.Status });
     }
 }
