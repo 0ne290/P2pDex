cab08e1bee072253697c9131766aed3a5ddd8cb9
One290 <lenya.dorovskoy@mail.ru>
2025-02-20 20:33:17 +0800

    cyvbii

--- Infrastructure.Persistence/Public/SellOrderAndItsTradersQuery.cs
+++ Infrastructure.Persistence/Public/SellOrderAndItsTradersQuery.cs
@@ -14,23 +14,18 @@ public class SellOrderAndItsTradersQuery : ISellOrderAndItsTradersQuery
         DbContext = dbContext;
     }
 
-    public async Task<SellOrderAndItsTradersDto?> Execute(Expression<Func<SellOrder, bool>> filter) => await DbContext
-        .SellOrders
-        .AsNoTracking().Where(filter)
-        .Join(DbContext.Traders, o => o.SellerId, t => t.Id,
-            (o, t) => new
-            {
-                status = o.Status, sellerId = t.Id, sellerName = t.Name, buyerId = o.BuyerId, crypto = o.Crypto,
-                cryptoAmount = o.CryptoAmount, fiat = o.Fiat, cryptoToFiatExchangeRate = o.CryptoToFiatExchangeRate,
-                fiatAmount = o.FiatAmount, paymentMethodInfo = o.PaymentMethodInfo
-            }).Join(DbContext.Traders, o => o.buyerId, t => t.Id,
-            (o, t) => new SellOrderAndItsTradersDto
-            {
-                Status = o.status, SellerId = o.sellerId, SellerName = o.sellerName, BuyerId = o.buyerId,
-                BuyerName = t.Name, Crypto = o.crypto, CryptoAmount = o.cryptoAmount, Fiat = o.fiat,
-                CryptoToFiatExchangeRate = o.cryptoToFiatExchangeRate, FiatAmount = o.fiatAmount,
-                PaymentMethodInfo = o.paymentMethodInfo
-            }).DefaultIfEmpty().FirstOrDefaultAsync();
+    public async Task<SellOrderAndItsTradersDto?> Execute(Expression<Func<SellOrder, bool>> filter)
+        => await DbContext.Database
+            .SqlQuery<SellOrderAndItsTradersDto>(
+                $"""
+                 SELECT SellOrders.Status, SellOrders.SellerId, Sellers.Name, SellOrders.BuyerId, Buyers.Name,
+                        SellOrders.Crypto, SellOrders.CryptoAmount, SellOrders.Fiat, SellOrders.CryptoToFiatExchangeRate,
+                        SellOrders.FiatAmount, SellOrders.PaymentMethodInfo
+                 FROM SellOrders
+                     INNER JOIN Traders Sellers ON SellOrders.SellerId = Sellers.Id
+                     LEFT JOIN Traders Buyers ON SellOrders.BuyerId = Buyers.Id;
+                 """
+            ).FirstOrDefaultAsync();
     
     public readonly P2PDexDbContext DbContext;
 }
